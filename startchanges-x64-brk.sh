1c715c7aa5f8c5b478a4dd8b6d03a02be7eecd95
mews-se <58894405+mews-se@users.noreply.github.com>
2024-07-04 11:23:40 +0200

    Overhaul (#17)
    * Create startchanges-x64-beta.sh
    
    * Update startchanges-x64-beta.sh
    
    * Update startchanges-x64-beta.sh
    
    * Update startchanges-x64-beta.sh
    
    * Update startchanges-x64-beta.sh
    
    * adding docker repo and docker
    
    * adjusted aliases and docker
    
    * Create startchanges-x64-new.sh
    
    * adjusting description and minor changes
    
    * sourcing bashfiles
    
    * fine tuning sourcing
    
    * complete overhaul
    
    * Create README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md

--- startchanges-x64-brk.sh
+++ startchanges-x64-brk.sh
@@ -2,19 +2,45 @@
 
 # Author: mews_se
 # Description:
-#   This comprehensive Bash script automates several system configuration tasks,
-#   enhancing user experience and security. It covers the configuration of sudoers,
-#   SSH settings, and the generation of an SSH key pair without a passphrase.
-#   Additionally, the script creates or updates .bashrc and .bash_aliases files with
-#   customized configurations and aliases to streamline command-line interactions.
-
+#   This Bash script automates several system configuration tasks
+#   to enhance user experience and bolster security. It begins by updating
+#   system packages to ensure the latest security patches and software
+#   enhancements are applied. The script then proceeds to configure sudo
+#   permissions, optimizing user management and system administration.
+#   
+#   SSH settings are adjusted to restrict root login and define
+#   specific users allowed SSH access, improving overall system security.
+#   Additionally, the script generates an SSH key pair without a passphrase,
+#   facilitating secure authentication between systems.
+#   
+#   To streamline command-line interactions, the script customizes the .bashrc
+#   and .bash_aliases files. These configurations include personalized prompts,
+#   command history settings, and useful aliases for frequently used commands.
+#   
+#   Beyond basic system setup, the script extends its functionality to include
+#   the installation and configuration of SNMPD (Simple Network Management
+#   Protocol Daemon) and Docker. SNMPD facilitates network monitoring by
+#   providing access to system metrics, while Docker simplifies application
+#   deployment through containerization technology.
+# 
 # Disclaimer:
-#   This script is customized for personal use by me. It is strongly recommended that
-#   users carefully review the code and understand its functionality before
-#   executing any commands. Use at your own risk.
+#   This script is authored and maintained for personal use by the author. Users
+#   are encouraged to carefully review the code and comprehend its operations
+#   before executing any commands. It is advisable to test the script in a
+#   controlled, non-production environment to evaluate its impact and
+#   functionality specific to your system configuration.
+#   
+#   The author assumes no responsibility for any unintended consequences,
+#   including but not limited to data loss, system downtime, or security
+#   breaches, resulting from the use of this script. By executing the script,
+#   users acknowledge and accept these risks.
+#   
+#   For critical systems or environments with unique configurations, consider
+#   adapting the script to suit specific requirements and conducting thorough
+#   testing prior to deployment.
 
 # List of required commands
-required_commands=("sudo" "apt-get" "sed" "ssh-keygen" "systemctl" "dpkg")
+required_commands=("sudo" "apt" "sed" "ssh-keygen" "systemctl" "dpkg" "curl")
 
 # Check if all required commands are available
 for cmd in "${required_commands[@]}"; do
@@ -37,13 +63,13 @@ system_update_upgrade() {
     log "Running system update and upgrade."
 
     # Update package lists
-    if ! sudo apt-get update; then
+    if ! sudo apt update; then
         log "Error: Failed to update package lists."
         exit 1
     fi
 
     # Perform a full upgrade
-    if ! sudo apt-get full-upgrade -y; then
+    if ! sudo apt full-upgrade -y; then
         log "Error: Failed to perform full upgrade."
         exit 1
     fi
@@ -130,12 +156,18 @@ create_bashrc() {
     log "Creating/updating .bashrc file."
     BASHRC_FILE="/home/$SUDO_USER/.bashrc"
 
+    # Remove .bashrc if it exists
+    if [ -f "$BASHRC_FILE" ]; then
+        sudo -u $SUDO_USER rm "$BASHRC_FILE"
+        log "Removed existing .bashrc file."
+    fi
+
     # Ensure home directory exists
-    sudo -u $SUDO_USER mkdir -p $(dirname "$BASHRC_FILE")
+    sudo -u $SUDO_USER mkdir -p "$(dirname "$BASHRC_FILE")"
 
     # .bashrc content
-    cat <<'EOL' | sudo -u $SUDO_USER tee -a "$BASHRC_FILE" > /dev/null
-case $- in
+    cat <<EOL | sudo -u $SUDO_USER tee -a "$BASHRC_FILE" > /dev/null
+case \$- in
     *i*) ;;
     *) return;;
 esac
@@ -146,17 +178,17 @@ HISTSIZE=1000
 HISTFILESIZE=2000
 shopt -s checkwinsize
 
-if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
-    debian_chroot=$(cat /etc/debian_chroot)
+if [ -z "\${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
+    debian_chroot=\$(cat /etc/debian_chroot)
 fi
 
-case "$TERM" in
+case "\$TERM" in
     xterm-color|*-256color) color_prompt=yes;;
 esac
 
 force_color_prompt=yes
 
-if [ -n "$force_color_prompt" ]; then
+if [ -n "\$force_color_prompt" ]; then
     if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
         color_prompt=yes
     else
@@ -164,23 +196,23 @@ if [ -n "$force_color_prompt" ]; then
     fi
 fi
 
-if [ "$color_prompt" = yes ]; then
-    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w \$\[\033[00m\] '
+if [ "\$color_prompt" = yes ]; then
+    PS1='\${debian_chroot:+(\$debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w \$\[\033[00m\] '
 else
-    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+    PS1='\${debian_chroot:+(\$debian_chroot)}\u@\h:\w\$ '
 fi
 unset color_prompt force_color_prompt
 
-case "$TERM" in
+case "\$TERM" in
 xterm*|rxvt*)
-    PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+    PS1="\[\e]0;\${debian_chroot:+(\$debian_chroot)}\u@\h: \w\a\]\$PS1"
     ;;
 *)
     ;;
 esac
 
 if [ -x /usr/bin/dircolors ]; then
-    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    test -r ~/.dircolors && eval "\$(dircolors -b ~/.dircolors)" || eval "\$(dircolors -b)"
     alias ls='ls --color=auto'
     alias grep='grep --color=auto'
     alias fgrep='fgrep --color=auto'
@@ -188,24 +220,28 @@ if [ -x /usr/bin/dircolors ]; then
 fi
 
 if [ -f ~/.bash_aliases ]; then
-    . ~/.bash_aliases
+    source ~/.bash_aliases
 fi
 EOL
 
     log ".bashrc file created/updated successfully for user: $SUDO_USER."
 }
 
+# Function to create or update .bash_aliases file
 create_bash_aliases() {
     log "Creating/updating .bash_aliases file."
     BASH_ALIASES_FILE="/home/$SUDO_USER/.bash_aliases"
 
+    # Remove .bash_aliases if it exists
+    if [ -f "$BASH_ALIASES_FILE" ]; then
+        sudo -u $SUDO_USER rm "$BASH_ALIASES_FILE"
+        log "Removed existing .bash_aliases file."
+    fi
+
     # List of aliases to be added
     aliases_to_add=$(cat <<'EOL'
 alias apta="sudo apt update && sudo apt full-upgrade && sudo apt autoremove -y && sudo apt clean"
-alias kodipi="ssh dietpi@10.0.0.7"
 alias sen="watch -n 1 sensors"
-alias brkpi="ssh dietpi@192.168.1.3"
-alias optiplex="ssh mews@192.168.1.6"
 alias reb="sudo reboot"
 alias dcupd="docker compose up -d"
 alias dcupdlog="docker compose up -d && docker compose logs -f"
@@ -213,34 +249,33 @@ alias dclog="docker compose logs -f"
 alias dcpull="docker compose pull"
 alias dcstop="docker compose stop"
 alias dcdown="docker compose down"
-alias mm="ssh martin@10.0.0.11"
-alias flight="ssh -p 2222 root@flight.rymdfartstyrelsen.se"
-alias prox="ssh root@10.0.0.99"
 alias fanoff="sudo systemctl stop fancontrol.service"
 alias fanon="sudo systemctl start fancontrol.service"
-alias tb="ssh 10.0.0.97"
+alias kodipi="ssh dietpi@10.0.0.7"
+alias dellpi="ssh dietpi@10.0.0.6"
+alias brkpi="ssh dietpi@192.168.1.3"
+alias optiplex="ssh mews@192.168.1.6"
+alias optilight="ssh dietpi@192.168.1.31"
+alias pfsensebrk="ssh -p 2221 admin@192.168.1.1"
+alias pfsense="ssh -p 2221 admin@10.0.0.1"
+alias newyork="ssh dietpi@newyork.stockzell.se"
+alias flight="ssh root@192.168.1.123"
+alias london="ssh dietpi@london.stockzell.se"
+alias nyc="ssh dietpi@newyork.stockzell.se"
+alias ned="ssh dietpi@ned.stockzell.se"
+alias tb="ssh dietpi@10.0.0.97"
+alias prox="ssh root@10.0.0.99"
+alias proxtor="ssh dietpi@10.0.0.97"
+alias teslamate="ssh dietpi@10.0.0.14"
+alias testpi="ssh dietpi@10.0.0.8"
+alias ff="fastfetch -c all.jsonc"
 EOL
 )
 
-    # Check if .bash_aliases file already exists
-    if [ -f "$BASH_ALIASES_FILE" ]; then
-        # Read existing content
-        existing_content=$(sudo -u $SUDO_USER cat "$BASH_ALIASES_FILE")
+    # If .bash_aliases file doesn't exist, create it with the provided content
+    echo "$aliases_to_add" | sudo -u $SUDO_USER tee "$BASH_ALIASES_FILE" > /dev/null
 
-        # Add aliases only if not present
-        while read -r alias_line; do
-            if ! grep -q "$alias_line" <<< "$existing_content"; then
-                echo "$alias_line" | sudo -u $SUDO_USER tee -a "$BASH_ALIASES_FILE" > /dev/null
-            fi
-        done <<< "$aliases_to_add"
-
-        log ".bash_aliases file updated successfully for user: $SUDO_USER."
-    else
-        # If .bash_aliases file doesn't exist, create it with the provided content
-        echo "$aliases_to_add" | sudo -u $SUDO_USER tee "$BASH_ALIASES_FILE" > /dev/null
-
-        log ".bash_aliases file created successfully for user: $SUDO_USER."
-    fi
+    log ".bash_aliases file created/updated successfully for user: $SUDO_USER."
 }
 
 # Function to install and configure SNMPD
@@ -309,6 +344,38 @@ EOL
     fi
 }
 
+# Function to install Docker repository
+install_docker_repository() {
+    log "Installing Docker repository."
+
+    # Add Docker's official GPG key:
+    sudo apt update
+    sudo apt install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt update
+
+    log "Docker repository installed successfully."
+}
+
+# Function to install Docker CE and related tools
+install_docker_ce() {
+    log "Installing Docker CE and related tools."
+
+    # Install Docker CE and tools
+    sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras -y
+    sudo usermod -aG docker $SUDO_USER
+
+    log "Docker CE and tools installed successfully. User added to the group docker"
+}
+
 # Function to run all tasks
 run_all_tasks() {
     system_update_upgrade
@@ -318,22 +385,31 @@ run_all_tasks() {
     create_bashrc
     create_bash_aliases
     install_configure_snmpd
+    install_docker_repository
+    install_docker_ce
 }
 
 # Main menu function
 main_menu() {
     while true; do
-        echo "Select an option:"
-        echo "1. System Update and Upgrade"
-        echo "2. Update Sudoers"
-        echo "3. Configure SSH"
-        echo "4. Generate SSH Key"
-        echo "5. Create/Update .bashrc"
-        echo "6. Create/Update .bash_aliases"
-        echo "7. Install and Configure SNMPD"
-        echo "8. Run All"
-        echo "9. Exit"
-        read -rp "Enter your choice [1-9]: " choice
+        clear
+        echo "#####################################"
+        echo "#   Automated System Configuration  #"
+        echo "#####################################"
+        echo "Please select an option:"
+        echo "  1) System Update and Upgrade"
+        echo "  2) Update sudoers"
+        echo "  3) Configure SSH"
+        echo "  4) Generate SSH Key"
+        echo "  5) Create/Update .bashrc"
+        echo "  6) Create/Update .bash_aliases"
+        echo "  7) Install and Configure SNMPD"
+        echo "  8) Install Docker official repo"
+        echo "  9) Install Docker and relevant tools"
+        echo "  10) Run all tasks"
+        echo "  11) Exit"
+
+        read -rp "Enter your choice: " choice
 
         case $choice in
             1) system_update_upgrade ;;
@@ -343,10 +419,22 @@ main_menu() {
             5) create_bashrc ;;
             6) create_bash_aliases ;;
             7) install_configure_snmpd ;;
-            8) run_all_tasks ;;
-            9) log "Script execution completed."; exit 0 ;;
-            *) echo "Invalid choice. Please select a valid option." ;;
+            8) install_docker_repository ;;
+            9) install_docker_ce ;;
+            10) run_all_tasks ;;
+            11)
+                log "Script execution completed."
+                log "Please apply the following command manually to source both .bashrc and .bash_aliases files:"
+                echo " . /home/$SUDO_USER/.bashrc && . /home/$SUDO_USER/.bash_aliases"
+                echo "Alternatively, you can log out and log back in to start a new shell session."
+                exit 0
+                ;;
+            *)
+                echo "Invalid choice. Please select a valid option."
+                ;;
         esac
+
+        read -rp "Press Enter to continue..."
     done
 }
 
